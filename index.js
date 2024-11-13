511bb756bc679b426817eba098e71246d73cff07
fjchng <jiacheng.fupoh@gmail.com>
2024-11-13 16:38:00 +0800

    provided solution to interview question and updated test cases

--- index.js
+++ index.js
@@ -5,7 +5,78 @@ function convertTimeToWords(time) {
     return 'midnight';
   }
 
-  return 'half past eight';
+  if (time == '12:00') {
+    return 'midday'
+  }
+
+
+  const hour_map = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve"
+  }
+
+  const minutes_map = {
+    0: "o'clock",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "quarter",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    21: "twenty one",
+    22: "twenty two",
+    23: "twenty three",
+    24: "twenty four",
+    25: "twenty five",
+    26: "twenty six",
+    27: "twenty seven",
+    28: "twenty eight",
+    29: "twenty nine",
+    30: "half"
+  }
+
+  const hour = Number(time.split(':')[0]) // 12
+  const minutes = Number(time.split(':')[1]) // 30
+
+  let time_text = ""
+
+  if (minutes <= 30) {
+    if (minutes === 0) {
+      time_text = hour_map[hour] + minutes_map[minutes]
+    } else {
+      time_text = minutes_map[minutes] + " past " + hour_map[hour]
+    }
+  } else {
+    let minutes_to_next_hour = 60 - minutes
+    time_text = minutes_map[minutes_to_next_hour] + " to " + hour_map[hour + 1]
+  }
+
+  return time_text;
+
 }
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const  { convertTimeToWords } = require('./index');
+const { convertTimeToWords } = require('./index');
 
 describe('Time to words', () => {
   it('Handles midnight', () => {
@@ -14,5 +14,30 @@ describe('Time to words', () => {
   it('Handles times after 30 mins - 2:45', () => {
     const timeInWords = convertTimeToWords('2:45');
     expect(timeInWords).toBe('quarter to three');
+  });
+
+  it('Handles times after 13 mins - 2:13', () => {
+    const timeInWords = convertTimeToWords('2:13');
+    expect(timeInWords).toBe('thirteen past two');
+  });
+
+  it('Handles times after 59 mins - 2:59', () => {
+    const timeInWords = convertTimeToWords('2:59');
+    expect(timeInWords).toBe('one to three');
+  });
+
+  it('Handles times after 15 mins - 2:15', () => {
+    const timeInWords = convertTimeToWords('2:15');
+    expect(timeInWords).toBe('quarter past two');
+  });
+
+  it('Handles times - 12:00', () => {
+    const timeInWords = convertTimeToWords('12:00');
+    expect(timeInWords).toBe('midday');
+  });
+
+  it('Handles times after 33 mins - 2:33', () => {
+    const timeInWords = convertTimeToWords('2:33');
+    expect(timeInWords).toBe('twenty seven to three');
   });
 });
